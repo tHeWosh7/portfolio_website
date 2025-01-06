9edd804f4919b1dd21e8b9457e2d74ffebf23f74
Diwash Acharya <119520840+tHeWosh7@users.noreply.github.com>
2025-01-06 18:10:08 +0545

    +js,m_css,m_html(futr)

--- porfolio.js
+++ porfolio.js
@@ -1,4 +1,82 @@
+// js code for the running text on intro 
 var typed = new Typed('#element', {
     strings: ['Front-End Developer', 'UI/UX Designer','Video Editor','Graphics Designer'],
     typeSpeed: 35,
   });
+
+//js code to hide items under div on edu_section
+var div= document.getElementById("div1");
+function hideshow()
+{
+  if (div.style.display==="none")
+  {
+    div.style.display="block";
+  }
+  else
+  {
+    div.style.display="none";
+  }
+}
+var divv= document.getElementById("div2");
+function hideshow2()
+{
+  if (divv.style.display==="none")
+  {
+    divv.style.display="block";
+  }
+  else
+  {
+    divv.style.display="none";
+  }
+}
+var divvv= document.getElementById("div3");
+function hideshow3()
+{
+  if (divvv.style.display==="none")
+  {
+    divvv.style.display="block";
+  }
+  else
+  {
+    divvv.style.display="none";
+  }
+}
+
+//js code to rewrite button sign and reverse
+
+document.getElementById("plus").addEventListener("click",rewrite);
+function rewrite(){
+  if (div.style.display==="none"){
+  document.getElementById("plus").innerHTML="-";
+  }
+  else
+  {
+  document.getElementById("plus").innerHTML="+";
+  }
+}
+
+document.getElementById("plus2").addEventListener("click",rewrite2);
+function rewrite2(){
+  if (divv.style.display==="none"){
+  document.getElementById("plus2").innerHTML="-";
+  }
+  else
+  {
+  document.getElementById("plus2").innerHTML="+";
+  }
+}
+
+document.getElementById("plus3").addEventListener("click",rewrite3);
+function rewrite3(){
+  if (divvv.style.display==="none"){
+  document.getElementById("plus3").innerHTML="-";
+  }
+  else
+  {
+  document.getElementById("plus3").innerHTML="+";
+  }
+}
+
+
+
+
